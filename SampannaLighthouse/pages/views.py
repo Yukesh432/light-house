2baeb1ac3e0e625f6f57db7bc47dd2cf3e193105
sakshamR <rimalsaksham10@gmail.com>
2020-09-14 10:50:08 +0545

    updated login

--- SampannaLighthouse/pages/views.py
+++ SampannaLighthouse/pages/views.py
@@ -4,3 +4,9 @@ from products.models import Product, Category
 def index(request):
     
     return render(request, 'pages/index.html')
+def about(request):
+    
+    return render(request, 'pages/about.html')
+def contact(request):
+    
+    return render(request, 'pages/contact.html')
